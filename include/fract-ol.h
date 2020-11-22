41568eebb0007a50928218c4a667ddbcf8b58eb7
Nikolai <professor3222@gmail.com>
2020-11-22 16:02:17 +0300

    init struct

---
+++ include/fract-ol.h
@@ -0,0 +1,10 @@
+//
+// Created by professor on 11/22/20.
+//
+
+#ifndef FRACT_OL_FRACT_OL_H
+#define FRACT_OL_FRACT_OL_H
+
+#include "libft.h"
+
+#endif //FRACT_OL_FRACT_OL_H
---
+++ src/main.c
@@ -0,0 +1,8 @@
+//
+// Created by professor on 11/22/20.
+//
+#include "fract-ol.h"
+
+int 	main(int argc, char** argv) {
+	return 0;
+}
