fb6c4f2b3357c9fa5f919daa7a352da034996731
Nikolai <professor3222@gmail.com>
2020-12-13 22:37:25 +0300

    adding window + image creation

--- includes/fract-ol.h
+++ includes/fract-ol.h
@@ -8,4 +8,24 @@
 #include "libft.h"
 #include "mlx.h"
 
+#define WIDTH 1000
+#define HEIGHT 1000
+
+typedef struct	s_image
+{
+	void	*img_ptr;
+	int		bits_per_pixel;
+	int		size_line;
+	int		endian;
+	int		*data;
+}				t_image;
+
+typedef struct s_fractol {
+	void *mlx_ptr;
+	void *win_ptr;
+	t_image image;
+	size_t screen_width;
+	size_t screen_length;
+}			   t_fractol;
+
 #endif //FRACT_OL_FRACT_OL_H
--- src/main.c
+++ src/main.c
@@ -3,9 +3,34 @@
 //
 #include "fract-ol.h"
 
+void hooks(t_fractol* fractol) {
+	mlx_loop(fractol->mlx_ptr);
+}
+
+t_fractol *init() {
+	t_fractol* fractol;
+
+	if (!(fractol = (t_fractol*)ft_memalloc(sizeof(t_fractol))))
+		return NULL;
+	fractol->mlx_ptr = mlx_init();
+	fractol->win_ptr = mlx_new_window(fractol->mlx_ptr,
+								      WIDTH, HEIGHT,
+								      "FRACT-OL");
+	fractol->image.img_ptr = mlx_new_image(fractol->mlx_ptr, WIDTH, HEIGHT);
+	fractol->image.data =  (int*)mlx_get_data_addr(fractol->image.img_ptr,
+												   &fractol->image.bits_per_pixel,
+												   &fractol->image.size_line,
+												   &fractol->image.endian);
+
+	return fractol;
+}
+
 int 	main(int argc, char** argv) {
-	void* mlx_ptr = mlx_init();
-	mlx_new_window(mlx_ptr, 100, 100, "chlen");
-	ft_printf("worked!");
+	t_fractol* fractol;
+
+	fractol = init();
+	ft_bzero(fractol->image.data, WIDTH * HEIGHT* sizeof(int));
+	mlx_put_image_to_window(fractol->mlx_ptr, fractol->win_ptr, fractol->image.img_ptr, 0, 0);
+	hooks(fractol);
 	return 0;
 }
